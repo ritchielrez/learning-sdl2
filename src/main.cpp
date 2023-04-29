1d0748b24bbb5367f0e6b57033178b2126c86219
RitchielRez <ritchielrez@gmail.com>
2023-04-29 14:29:02 +0600

    feat(game): game version set to 0.1

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,6 @@
 int main(int argc, char **argv)
 {
     std::unique_ptr<Game> game = std::make_unique<Game>();
-    game->init("Game v1.0", game->windowWidth, game->windowHeight);
+    game->init("Game v0.1", game->windowWidth, game->windowHeight);
     return 0;
 }
